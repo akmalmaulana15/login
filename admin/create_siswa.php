420a1fab017522051e52277ea5438d82c66590fe
akmalmaulana <muhammad_akmal_29rpl@student.smktelkom-mlg.sch.id>
2021-11-28 19:11:21 +0700

    login

---
+++ admin/create_siswa.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" countent="IE=edge">
+    <meta name="vieport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="create_siswa.post.php" method="post">
+    <div>
